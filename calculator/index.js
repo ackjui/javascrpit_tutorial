ed1b2b6be0c7cf31d8563fc26d3151055ad24adf
rawat <rawat@rawat.168.1.11>
2023-04-11 23:38:45 +0530

    initial commit

---
+++ calculator/index.js
@@ -0,0 +1,25 @@
+let num1 = 8;
+let num2 = 2;
+
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
+
+function add() {
+    let sum = num1 + num2;
+    document.getElementById("sum-el").textContent = sum;
+}
+
+function subtract() {
+    let sub = num1 - num2;
+    document.getElementById("sum-el").textContent = sub;
+}
+
+function divide() {
+    let div = num1/num2;
+    document.getElementById("sum-el").textContent = div;
+}
+
+function multiply() {
+    let mul = num1 * num2;
+    document.getElementById("sum-el").textContent = mul;
+}
---
+++ render_error_message/index.js
@@ -0,0 +1,4 @@
+function purchase() {
+  let para_el = document.getElementById("error");
+  para_el.innerText = "Something went wrong, please try again.";
+}
